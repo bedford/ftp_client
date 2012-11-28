bb3909efa0bef9476f4564f069cb9a34e3769931
bedford <bedford2001@gmail.com>
2012-11-28 10:30:11 +0800

    socket - modify connect and write functions to avoid complex process logic
     <1>修改socket中的socket_connect和socket_write函数,避免过多的判断逻辑嵌套;
     <2>头文件socket.h增加doxgen格式的注释信息;

--- socket.c
+++ socket.c
@@ -8,8 +8,6 @@
 
 #include "socket.h"
 
-static int kConnectTimeout = 2000;
-
 int socket_create_tcp(void)
 {
         int socket_fd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -101,16 +99,17 @@ int socket_connect(int socket_fd, char *dest_addr, int dest_port)
                 return SOCKET_FAIL;
         }
 
-        if (!socket_check_writable(socket_fd, kConnectTimeout)) {
-                int error = 0;
-                socklen_t len = sizeof(error);
-                getsockopt(socket_fd, SOL_SOCKET, SO_ERROR, &error, &len);
-
-                if (error == 0) {
-                        return SOCKET_OK;
-                }
-
+        int connect_timeout = 2000;
+        if (socket_check_writable(socket_fd, connect_timeout)) {
                 return SOCKET_FAIL;
+        }
+
+        int error = 0;
+        socklen_t len = sizeof(error);
+        getsockopt(socket_fd, SOL_SOCKET, SO_ERROR, &error, &len);
+
+        if (error == 0) {
+                return SOCKET_OK;
         }
 
         return SOCKET_FAIL;
@@ -124,19 +123,19 @@ int socket_write(int socket_fd, char *buf, int length, int ms)
 
         while (sent_len < length) {
                 ret = socket_check_writable(socket_fd, ms);
-                if (ret == SOCKET_OK) {
-                        n = send(socket_fd, buf + sent_len,
-                                        length - sent_len, 0);
-                        if (n == -1) {
-                                return SOCKET_FAIL;
-                        } else if (n == 0) {
-                                return SOCKET_FAIL;     //copy to send buffer fail
-                        }
-
-                        sent_len += n;
-                } else {
+                if (ret != SOCKET_OK) {
                         return ret;
                 }
+
+                n = send(socket_fd, buf + sent_len,
+                                length - sent_len, 0);
+                if (n == -1) {
+                        return SOCKET_FAIL;
+                } else if (n == 0) {
+                        return SOCKET_FAIL;     //copy to send buffer fail
+                }
+
+                sent_len += n;
         }
 
         return SOCKET_OK;
@@ -157,7 +156,7 @@ int socket_read(int socket_fd, char *buf, int max_length, int ms)
         return length;
 }
 
-int socket_clear_recv_buffer(int socket_fd)
+void socket_clear_recv_buffer(int socket_fd)
 {
         int ret = 0;
         int buf_size = 256;
@@ -169,8 +168,6 @@ int socket_clear_recv_buffer(int socket_fd)
                         break;
                 }
         }
-
-        return SOCKET_OK;
 }
 
 int socket_close(int socket_fd)
--- socket.h
+++ socket.h
@@ -10,12 +10,65 @@ extern "C" {
 #define SOCKET_TIMEOUT  -2
 
 int socket_create_tcp(void);
+
 void socket_set_nonblock(int socket_fd);
+
 void socket_set_block(int socket_fd);
+
+
+/**
+ * @brief       connect to the dest_addr host, dest_port 
+ *
+ * @param       socket_fd [I ] client socket file description
+ * @param       dest_addr [I ] destination host address
+ * @param       dest_port [I ] destination port
+ *
+ * @return      result of connecting
+ * @retval      -1      fail
+ * @retval      0       success
+ */
+
 int socket_connect(int socket_fd, char *dest_addr, int dest_port);
+
+
+/**
+ * @brief       socket_check_writable To check whether the socket is writable
+ *
+ * @param       socket_fd [I ] The socket file description to check
+ * @param       ms      [I ]   timeout up bound (Unit: ms)
+ *
+ * @return      result of able to write or not
+ * @retval      -2      timeout error
+ * @retval      -1      unwritable
+ * @retval      0       writable
+ */
+
 int socket_write(int socket_fd, char *buf, int length, int ms);
+
+
+/**
+ * @brief       socket_check_readable To check whether the socket is readable
+ *
+ * @param       socket_fd [I ] The socket file description to check
+ * @param       ms      [I ]   timeout up bound (Unit: ms)
+ *
+ * @return      result of able to read or not
+ * @retval      -2      timeout error
+ * @retval      -1      unreadable
+ * @retval      0       readable
+ */
+
 int socket_read(int socket_fd, char *buf, int max_length, int ms);
-int socket_clear_recv_buffer(int socket_fd);
+
+/**
+ * @brief       socket_clear_recv_buffer To clear socket receive buffer
+ *
+ * @param       socket_fd [I ] The socket file description to clear
+ */
+
+void socket_clear_recv_buffer(int socket_fd);
+
+
 int socket_close(int socket_fd);
 
 #ifdef __cplusplus
